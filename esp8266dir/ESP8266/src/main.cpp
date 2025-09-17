20f2d2b32e9a7cc9f1acbcf7802c2f4513b04e68
d.kunicin <d.kunucun@looch.ru>
2025-09-17 10:46:31 +0700

    added blink

--- esp8266dir/ESP8266/src/main.cpp
+++ esp8266dir/ESP8266/src/main.cpp
@@ -8,12 +8,21 @@ const char* _PASSWORD_STA = "20081111";
 
 MyClassEsp8266 CustomWiFiModule(_LOGIN_AP, _PASSWORD_AP, _LOGIN_STA, _PASSWORD_STA);
 
+#ifndef LED_BUILTIN
+#define LED_BUILTIN
+#endif
+bool blink = true;
 void setup() {
     Serial.begin(9600);
     // CustomWiFiModule.setupingTwoModes();
     CustomWiFiModule.setupWiFiApMode();
     CustomWiFiModule.serverOn();
+
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+    digitalWrite(LED_BUILTIN, blink);
+    blink = !blink;
+    delay(1000);
 }
